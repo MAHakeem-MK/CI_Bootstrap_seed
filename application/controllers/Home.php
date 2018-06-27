fe8bdd1d16310f2121ef44870a894d7b673aaac1
mahakeemmk <dev.mahakeemmk@gmail.com>
2018-06-27 11:29:48 +0530

    **seed **bootstrap integrated,structured diroctories.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('template/header');
 		$this->load->view('home_view');
+		$this->load->view('template/footer');
 	}
 }
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,4 +1,5 @@
 <center>
-    <img src="<?php echo asset_url();?>/gears-animation.gif" alt="loading.." width="180" height="180"/>
+    <img src="<?php echo asset_url();?>/icons/gears-animation.gif" alt="loading.." width="180" height="180"/>
     <h3>web site is in construction!</h3>
 </center>
+
